c4ec3ffeac5d2aef54573d4aae1078cc9acdc92e
BlueFeet <b1uefeet.dev@gmail.com>
2023-06-09 10:10:10 -0500

    Ternario

--- src/com/funcional/Main.java
+++ src/com/funcional/Main.java
@@ -8,36 +8,43 @@ public class Main {
     public static void main(String[] args) {
 
     Lista<Integer> miLista = Lista.of(2,3,7,4,8);
-        System.out.println( miLista.head() );
-        System.out.println(miLista.toString());
-        var empty =Lista.of();
+        System.out.println("LISTA:");
+        System.out.println(miLista.toString()+ "\n");
+        
+        System.out.println("APPEND");
         var l2 = miLista.append(99);
-        System.out.println((l2));  
+        System.out.println(l2 + "\n");  
+        
+        System.out.println("PREPEND");
         var l3 = miLista.prepend(99);
-        System.out.println((l3));
-        var l4 = miLista.remove(5);
-        System.out.println((l4));
+        System.out.println(l3+ "\n");
+        
+        System.out.println("REMOVE");
+        var l4 = miLista.remove(3);
+        System.out.println((l4)+ "\n");
         
         
         System.out.println("DROP");
         var l5 = miLista.drop(2);
-        System.out.println(l5);
+        System.out.println(l5+ "\n");
         
         System.out.println("DROPWHILE");
+        var l10 = Lista.of();
         var l6 = miLista.dropWhile(t->t%2==0);
-        System.out.println(l6);
+        System.out.println(l6+ "\n");
+        System.out.println(l10+ "\n");
         
         System.out.println("CONCAT");
         var l7 = miLista.concat(miLista);
-        System.out.println(l7);
+        System.out.println(l7+ "\n");
         
         System.out.println("TAKE");
         var l8 = miLista.take(2);
-        System.out.println(l8);
+        System.out.println(l8+ "\n");
                 
         System.out.println("TAKEWHILE");
         var l9 = miLista.takeWile(t->t%2==0);
-        System.out.println(l9);
+        System.out.println(l9+ "\n");
         
         
 
--- src/com/funcional/lista/Lista.java
+++ src/com/funcional/lista/Lista.java
@@ -2,97 +2,60 @@ package com.funcional.lista;
 
 import java.util.function.Predicate;
 
-public sealed interface Lista<T> permits  Nil, Cons {
+public sealed interface Lista<T> permits Nil, Cons {
 
-    Lista NIL = new Nil();
+	Lista NIL = new Nil();
 
+	T head();
 
+	Lista<T> tail();
 
-    T head();
-    Lista<T> tail( );
+	boolean isEmpty();
 
-    boolean isEmpty();
+	static <T> Lista<T> of(T h, Lista<T> t) {
+		return new Cons<>(h, t);
+	}
 
-    static <T> Lista< T> of(T h, Lista<T> t){
-        return new Cons<>(h,t);
-    }
-    static <T> Lista<T> of(T ... elems) {
-        Lista<T> ret = Lista.NIL;
-        for(int i = elems.length -1; i>=0; i--){
-            T h = elems [i];
-            var tmp = Lista.of(h,ret);
-            ret = tmp;
-        }
-        return ret;
-    }
+	static <T> Lista<T> of(T... elems) {
+		Lista<T> ret = Lista.NIL;
+		for (int i = elems.length - 1; i >= 0; i--) {
+			T h = elems[i];
+			var tmp = Lista.of(h, ret);
+			ret = tmp;
+		}
+		return ret;
+	}
 
-    default Lista<T> append (T elemen){
-        if (isEmpty()){
-            return Lista.of(elemen);
-        }else{
-            return Lista.of(head(), tail().append(elemen));
-        }
-    }
+	default Lista<T> append(T elemen) {
+		return isEmpty() ? Lista.of(elemen) : Lista.of(head(), tail().append(elemen));
+	}
 
-    default Lista<T> prepend(T elem){
-        return Lista.of(elem,this);
-    }
-    
-    default Lista<T> remove(T elem){
-    	if (isEmpty()) {
-            return NIL;
-    	}else {
-            if (elem.equals(head())){
-                return tail();
-            }else {
-                return Lista.of(head(),tail().remove(elem));
-            }
-        }
-    }
-    
-    default Lista<T> drop(int n){
-        //elimina un numero determinado de elemntos drop(2)  // elimina dos elementos
-        if (isEmpty() || n<=0){
-            return this;
-        }else{
-                return tail().drop(n-1); 
-        }
-                       
-    }
-    
-   default Lista<T> dropWhile(Predicate <T> p){
-        if (isEmpty() || p.test(head())){
-            return this;
-        }else{
-                return tail().dropWhile(p); 
-        }
-   }
-   
-   default Lista<T> concat(Lista<T> a){
-       if ( isEmpty()){
-           return a;
-       }else{
-           return Lista.of(head(), tail().concat(a));
-       }
-   }
-   
-   default Lista<T> take(int n){
-       if ( isEmpty() || n<=0){
-           return NIL;
-       }else{
-          return Lista.of(head(), tail().take(n-1));
-       }
-   }
-      default Lista<T> takeWile(Predicate <T> p){
-       if ( isEmpty() || !p.test(head())){
-           return NIL;
-       }else{
-          return Lista.of(head(), tail().takeWile(p));
-       }
-   }
-   
+	default Lista<T> prepend(T elem) {
+		return Lista.of(elem, this);
+	}
 
+	default Lista<T> remove(T elem) {
+		return isEmpty() ? NIL : isEmpty() ? tail() : Lista.of(head(), tail().remove(elem));
+	}
 
+	default Lista<T> drop(int n) {
+		// elimina un numero determinado de elemntos drop(2) // elimina dos elementos
+		return isEmpty() || n <= 0 ? this : tail().drop(n - 1);
+	}
 
+	default Lista<T> dropWhile(Predicate<T> p) {
+		return isEmpty() || p.test(head()) ? tail().dropWhile(p) : this;
+	}
+
+	default Lista<T> concat(Lista<T> a) {
+		return isEmpty() ? a : Lista.of(head(), tail().concat(a));
+	}
+
+	default Lista<T> take(int n) {
+		return isEmpty() || n < 0 ? NIL : Lista.of(head(), tail().take(n - 1));
+	}
+
+	default Lista<T> takeWile(Predicate<T> p) {
+		return isEmpty() || !p.test(head()) ? NIL : Lista.of(head(), tail().takeWile(p));
+	}
 }
-
