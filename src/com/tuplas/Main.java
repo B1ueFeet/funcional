09b3e08f184cccb06d1c6bfe7e7191a687402a66
BlueFeet <b1feet.dev@uce.edu.ec>
2023-06-22 09:59:56 -0500

    version alterna

--- src/com/tuplas/Main.java
+++ src/com/tuplas/Main.java
@@ -1,5 +1,6 @@
-package com.funcional;
+package com.tuplas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -7,31 +8,27 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.funcional.Compo;
+
 public class Main {
 	public static void main(String[] args) {
 		// dado un nString contar el numero de repeticiones de cada palabra 
 		
 		Function<String, String[]> f = a -> a.split(" ");
-		
-		Function<String[], List<Tupla<String, Integer>>> g = b ->{
-			Set<String> set = new HashSet<>(Arrays.asList(b));
-			List<Tupla<String, Integer>> lista = set.stream().map(c -> Tupla.<String,Integer>getTupla(b,c)).collect(Collectors.toList());
-			return lista;
-		};
-		
-	
 		var a = f.apply("hola mundo en java hola hola");
 		
-		for (int i = 0; i< a.length; i++ ) {
-			System.out.println(a[i]);
-		}
+		System.out.println("hola: " + Tupla.cantidadPalabra("hola", a, 0,0) );
+		
+		Function<String[], List<Tupla<String, Integer>>> g = b -> {
+			List<Tupla<String, Integer>> result = new ArrayList<>();
+			return Tupla.getList(b, 0, result);
+		};
 		
 		var d = g.apply(a);
 		
-		d.stream().forEach(System.out::println);
+		System.out.println(d);
 		
-		System.out.println("**************************************");
-
+		d.stream().forEach(System.out::println);
 		
 		Function<String, List<Tupla<String, Integer>> > comp = Compo.<String,String[], List<Tupla<String, Integer>> >composicion2().apply(f).apply(g);
 		System.out.println(comp.apply("hola mundo en java hola hola mundo en hola java"));
